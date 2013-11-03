18926afd21350bab2c0c1830fd6d815d5c52c0b9
AtoCrafter <ato.crafter@gmail.com>
2013-11-03 18:26:54 +0900

    ResourceLocation のパスを cpw/ironchest を参考に修正

--- common/ato/markerinstaller/GuiInstaller.java
+++ common/ato/markerinstaller/GuiInstaller.java
@@ -67,7 +67,7 @@ public class GuiInstaller extends GuiContainer {
     @Override
     protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3) {
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        mc.getTextureManager().bindTexture(new ResourceLocation("markerinstaller:/textures/gui/installer.png"));
+        mc.getTextureManager().bindTexture(new ResourceLocation("markerinstaller", "textures/gui/installer.png"));
         int var5 = (width - xSize) / 2;
         int var6 = (height - ySize) / 2;
         drawTexturedModalRect(var5, var6, 0, 0, xSize, ySize);
